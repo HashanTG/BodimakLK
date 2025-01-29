40750c49be3c25e5eb16b36afd350deee1c93467
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2025-01-29 13:22:36 +0530

    this is the folder structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <motion.div
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: 1, x: 0 }}
+        className="logo"
+      >
+        bodimak.lk
+      </motion.div>
+      <div className="buttons">
+        <button className="sign-in">Sign in</button>
+        <button className="sign-up">Sign up</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
